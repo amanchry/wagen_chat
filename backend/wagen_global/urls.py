316b8d0b3b3110b2012ccd2d271b816f7511b29d
Aman Chaudhary <amanchaudhary.web@gmail.com>
2025-10-17 12:36:36 +0200

    updated

--- backend/wagen_global/urls.py
+++ backend/wagen_global/urls.py
@@ -32,8 +32,8 @@ urlpatterns = [
 
 
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
+    urlpatterns += static("/wagenApi/media/",
                           document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL,
+    urlpatterns += static("/wagenApi/static/",
                           document_root=settings.STATIC_ROOT)
     
