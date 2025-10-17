629425637ade8c9ba9f63fc59810a8388d694537
Aman Chaudhary <amanchaudhary.web@gmail.com>
2025-10-17 12:44:21 +0200

    updated

--- backend/wagen_global/settings.py
+++ backend/wagen_global/settings.py
@@ -177,8 +177,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_URL = '/django_static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'django_static/')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
