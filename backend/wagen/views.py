7cd57658fb365cc2911d1677741cb3573e16afb7
Aman Chaudhary <amanchaudhary.web@gmail.com>
2025-11-13 20:16:04 +0100

    updated

--- backend/wagen/views.py
+++ backend/wagen/views.py
@@ -522,13 +522,26 @@ def getAddedAreasList(request):
 
 
 @csrf_exempt
-def get_task_status(request, task_id):
+def get_task_status(request):
     user, error = validate_jwt_request(request)
     if error:
         return error
     
-    print("task_id",task_id)
+    # print("task_id",task_id)
+    # task = AsyncResult(task_id)
+
+    last_task_entry = TaskHistory.objects.filter(user=user).order_by("-date").first()
+
+    if not last_task_entry:
+        return JsonResponse({
+            "state": "NO_TASK",
+            "status": "No tasks found for this user."
+        })
+
+    task_id = last_task_entry.task
     task = AsyncResult(task_id)
+
+
 
     # if task.state in ['SUCCESS', 'FAILURE']:
     #     mapset_name = f"job_{task_id}"
